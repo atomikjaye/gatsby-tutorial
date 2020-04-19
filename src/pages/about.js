346e9b3493dd8a29d27ef0af87142fb2b455a022
AtomikJaye <atomikjaye@gmail.com>
2020-04-19 19:05:11 -0400

    Gatsby Tutorial 1 - 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,23 @@
 import React from "react"
-import Header from "../components/header"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => (
-  <div style={{ color: `steelblue` }}>
-    <Header headerText="About JAYE" />
-    <p>About meeeeeeee!</p>
-    <ul>
-      <li>S</li>
-      <li>S</li>
-      <li>S</li>
-      <li>S</li>
-    </ul>
-  </div>
+export default ({ data }) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best
+      photos and videos of pandas eating lots of food.
+    </p>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
-export default () => (
-  <div style={{ color: `steelblue` }}>
-    <Link to="/contact">Contact</Link>
-    <Header headerText="Hello Jaye" />
-    <p>This is our awesome subwall.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>)
+export default ({ data }) => (
+  <Layout>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
+)
